40c1c1c30d46a66970cad33a173b47326e7df4ef
Randy Tavarez <80017046+RandyTG@users.noreply.github.com>
2022-01-30 21:52:19 -0500

    updated header to B v5

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function Header() {
   return (
-    <header className="jumbotron bg-dark">
-      <div className="container text-white">
+    <header className="navbar bg-dark mb-4 shadow p-3 mb-5">
+      <div className="container d-flex flex-column text-white">
         <h1 className="display-4">Flashcard-o-matic</h1>
         <p className="lead">Discover The Flashcard Difference.</p>
       </div>
